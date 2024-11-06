802b74efd06956cbddd968d83f7b94130486bf49
Manish Kumar <manish.login01@gmail.com>
2024-11-06 23:26:06 +0530

    Add React Router; Register.jsx, Login.jsx, HomePage.jsx, Dashboard.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  )
-}
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
