f9f89b3e064ec68ae8a83d77a78acc13617a04ec
Manish Kumar <manish.login01@gmail.com>
2025-03-28 14:54:44 +0530

    add Navbar, Login, Signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import './App.css'
-import Navbar from './components/shared/Navbar'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
+import Home from "./components/Home";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+]);
 
 const App = () => {
   return (
     <>
-      <Navbar/>
+      <RouterProvider router={appRouter} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -2,9 +2,12 @@ import React from "react";
 // import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { User2, LogOut } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const user = false;
   return (
     <div className="bg-white ">
       <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
@@ -22,20 +25,45 @@ const Navbar = () => {
           <li><Link>Jobs</Link></li>
           <li><Link>Browse</Link></li> */}
           </ul>
-
-          <Popover>
-            <PopoverTrigger asChild>
-              <Avatar className="cursor-pointer">
-                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-              </Avatar>
-            </PopoverTrigger>
-            <PopoverContent className="w-80">
-              <Avatar className="cursor-pointer">
-                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-              </Avatar>
-              <h4 className="font-medium ">Ethan Hunt</h4>
-            </PopoverContent>
-          </Popover>
+          {!user ? (
+            <div className="flex gap-2 items-center">
+              <Link to="/login">
+                <Button variant="primary">Login</Button>
+              </Link>
+              <Link to="/signup">
+                <Button className="bg-[#6A38C2] hover:bg-[#5b30a6]">Signup</Button>
+              </Link>
+            </div>
+          ) : (
+            <Popover>
+              <PopoverTrigger asChild>
+                <Avatar className="cursor-pointer">
+                  <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                </Avatar>
+              </PopoverTrigger>
+              <PopoverContent className="w-80">
+                <div className="flex gap-4 space-y-2">
+                  <Avatar className="cursor-pointer">
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                  </Avatar>
+                  <div>
+                    <h4 className="font-medium ">Ethan Hunt</h4>
+                    <p className="text-sm text-muted-foreground">Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+                  </div>
+                </div>
+                <div className="flex flex-col my-2 text-gray-600">
+                  <div className="flex w-fit items-center gap-2 cursor-pointer">
+                    <User2 />
+                    <Button variant="link">View Profile</Button>
+                  </div>
+                  <div className="flex w-fit items-center gap-2 cursor-pointer">
+                    <LogOut />
+                    <Button variant="link">Logout</Button>
+                  </div>
+                </div>
+              </PopoverContent>
+            </Popover>
+          )}
         </div>
       </div>
     </div>
