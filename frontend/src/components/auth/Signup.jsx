657928bc7d1380fb1aa1ad6567a2f5396e559203
Manish Kumar <manish.login01@gmail.com>
2025-03-29 15:43:43 +0530

    setup redux

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -8,6 +8,8 @@ import { Button } from "@/components/ui/button";
 import { Link, useNavigate } from "react-router-dom";
 import { USER_API_END_POINT } from "../../utils/constant";
 import { toast } from "sonner";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoading } from "../../redux/authSlice";
 
 const Signup = () => {
   const [input, setInput] = useState({
@@ -18,7 +20,9 @@ const Signup = () => {
     role: "",
   });
 
+  const { loading } = useSelector((store) => store.auth);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setInput({ ...input, [e.target.name]: e.target.value });
@@ -37,7 +41,9 @@ const Signup = () => {
     formData.append("password", input.password);
     formData.append("role", input.role);
     if (input.file) formData.append("file", input.file);
+
     try {
+      dispatch(setLoading(true));
       const res = await axios.post(`${USER_API_END_POINT}/register`, formData, {
         headers: {
           "Content-Type": "multipart/form-data",
@@ -51,6 +57,8 @@ const Signup = () => {
     } catch (error) {
       console.log(error);
       toast.error(error.response.data.message);
+    } finally {
+      dispatch(setLoading(false));
     }
   };
 
@@ -93,9 +101,15 @@ const Signup = () => {
             </div>
           </div>
 
-          <Button type="submit" className="w-full my-4">
-            Sign up
-          </Button>
+          {loading ? (
+            <Button className="w-full my-4" disabled>
+              Please wait..
+            </Button>
+          ) : (
+            <Button type="submit" className="w-full my-4">
+              Sign Up
+            </Button>
+          )}
           <span>
             Already have an account?{" "}
             <Link to="/login" className="text-blue-600 text-sm">
