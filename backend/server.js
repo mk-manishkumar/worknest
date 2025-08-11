db3138b15cbcc3c13f111e92f6a5a65eb5f9d3e7
Manish Kumar <manish.login01@gmail.com>
2025-08-11 22:51:39 +0530

    feat : add vercel.json configs for separate backend and frontend deployment

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,12 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) callback(null, true);
+    else callback(new Error("Not allowed by CORS"));
+  },
   credentials: true,
 };
 app.use(cors(corsOptions));
